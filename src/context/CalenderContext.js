5a2dee4f1b9ccc8b73db26d4fe0234bb492d863e
RanaFathy123 <ranafathy817@gmail.com>
2024-10-06 00:53:10 +0300

    ticket

--- src/context/CalenderContext.js
+++ src/context/CalenderContext.js
@@ -3,7 +3,7 @@
 import React, { createContext, useCallback, useState } from "react";
 import Swal from "sweetalert2";
 
-// Create a Context
+// Create a Context2
 export const CalendarContext = createContext(null);
 
 // Create a Provider Component
